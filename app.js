de5d8ec8cb2683b742de2c0700df7054d6950f79
MosesEtte0721 <ett20002@byui.edu>
2022-09-18 06:55:08 +0100

    REST api

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
 //import mongoose package
 const mongoose = require("mongoose");
+
 //import express package
 const express = require('express');
-const port = 3000;
-const host = 'localhost';
-
-//require('dotenv/config');
-
 const  app = express();
 
+const port =  3000;
+
+
+//access .env file
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+
  //connect to MongoDB
- mongoose.connect('mongodb+srv://MosesEtte:MosesEtte@cluster0.cr9frgg.mongodb.net/test', ()=>{console.log('connected to mongoDB')});
+ mongoose.connect(process.env.mongodb, ()=>{console.log('connected to mongoDB')});
 
 // creating server/router with express package
-app.get('/', (req, res) => {
-    res.send('FIRST MESSAGE ON THE SERVER type "/fullName" in the address box after the 3000 ', ()=>{console.log('we are now connected to mongoDB')});
-});
 
-app.get('/firstName', (req, res) => {
-    res.send('Jackson ');
-});
+app.get('/', (req, res)=>{
+    res.send('GIVEN THE SITUATION OF THINGS')
+})
 
-app.get('/middleName', (req, res) => {
-    res.send('Idongesit ');
-});
+app.get('/mName',(req, res)=>{
+    res.send('ARCHIBONG is the middle name. Type "/fName"')
+})
 
-app.get('/lastName', (req, res) => {
-    res.send('Udofot');
-});
-app.get('/fullName', (req, res) => {
-    res.send('Jackson Idongesit Udofot is my childhood best friend');
-});
+app.get('/lName',(req, res)=>{
+    res.send('ETTE is the last name. Type"/mName"')
+})
+
+app.get('/fName',(req, res)=>{
+    res.send(' MOSES ARCHIBONG ETTE full name')
+})
+
 
  app.listen(port,()=>{console.log('connected to the server')})
-
---
+++ routes/router.js
@@ -0,0 +1,27 @@
+//const express = require('express');
+//const router = express.Router();
+
+// creating server/router with express package
+//router.get('/', (req, res) => {
+  //  res.send("FIRST MESSAGE ON THE SERVER type (fullName in the address box after the 3000) ");
+//});
+
+/*router.get('/firstName', (req, res) => {
+    res.send('Jack')
+})
+
+router.get('/middleName', (req, res) => {
+    res.send('Idongesit ');
+});
+
+router.get('/lastName', (req, res) => {
+    res.send('Efiat');
+});
+*//*
+router.get('/fullName', (req, res) => {
+    res.send('Jack Idongesit Efiat is my childhood best friend');
+});
+router.listen(port,()=>{console.log('connected to the server')})
+
+module.exports = router;
+*/
